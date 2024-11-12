5dc988aacdd7ebfd540780afea64aa1b653ef050
SharonIV0x86 <anirudhlakhanpal123@gmail.com>
2024-11-12 20:25:45 +0530

    sum fix 2

--- backend/main.py
+++ backend/main.py
@@ -3,15 +3,13 @@ from ai import gen_docstring, gen_algorithm
 from flask import Flask, request, jsonify
 from fileIO import read_files, write_files
 from github_routes import clone_repo
-
 app = Flask(__name__)
 
-PUBLIC_DIR = "uploads"
-os.makedirs(PUBLIC_DIR, exist_ok=True)
 
 @app.route("/", methods=["GET"])
 def home():
     return "Hello World"
+
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
@@ -38,6 +36,7 @@ def upload_file():
 
     return {"files": output_file_records}, 200
 
+
 @app.route("/genalgo", methods=["POST"])
 def generate_algorithm():
     data = request.get_json()
@@ -45,8 +44,11 @@ def generate_algorithm():
         text = data["text"]
         gen_algorithm(text)
         return jsonify({"received_text": text}), 200
+    
     else:
         return jsonify({"error": "No text data provided"}), 400
+    
+    
 
 @app.route("/single", methods=["POST"])
 def single():
@@ -63,6 +65,7 @@ def cloneRepo():
 
     # Call the clone_repo function to clone the repo
     return clone_repo(repo_url)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
