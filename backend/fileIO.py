bc3391fc7b857cbddc9e11cc4ad704dd3c79ca88
adityanandanx <nandanaditya985@gmail.com>
2024-11-12 14:49:25 +0530

    chore: remove comments

--- backend/fileIO.py
+++ backend/fileIO.py
@@ -1,29 +1,23 @@
-# file_utils.py
 import os
 
-# Define the public directory to save files
-PUBLIC_DIR = 'public'
+PUBLIC_DIR = "public"
 
-# Ensure the public directory exists
 os.makedirs(PUBLIC_DIR, exist_ok=True)
+
 
 def write_to_file(file_records):
     for file_data in file_records:
-        # Extract the filename and content from the file data
         filename = file_data.get("filename")
         content = file_data.get("content")
 
         if not filename or not content:
             raise ValueError("Filename or content missing")
 
-        # Define the full path for the file in the public directory
         file_path = os.path.join(PUBLIC_DIR, filename)
 
         try:
-            # Ensure subdirectories in filename path are created
             os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-            # Write content to the file in the public directory
             with open(file_path, "w") as file:
                 file.write(content)
 
--- backend/main.py
+++ backend/main.py
@@ -5,31 +5,26 @@ from fileIO import write_to_file
 
 app = Flask(__name__)
 
-# Define the public directory to save files
 PUBLIC_DIR = "uploads"
 
-# Ensure the public directory exists
 os.makedirs(PUBLIC_DIR, exist_ok=True)
+
 
 @app.route("/", methods=["GET"])
 def home():
     return "Hello World"
 
+
 @app.route("/upload", methods=["POST"])
 def upload_file():
-    # Check if files are present in the request
     if "file" not in request.files:
         return "No file part in the request", 400
 
-    # Get the list of files
     files = request.files.getlist("file")
 
-    # Initialize a list to store file details
     file_records = []
 
-    # Process each file
     for file in files:
-        # Extract the filename and file extension
         if not file.filename:
             print("Warning: No file name, skipping file...")
             continue
@@ -37,21 +32,14 @@ def upload_file():
         filename: str = file.filename
         file_ext = filename.rsplit(".", 1)[-1] if "." in filename else ""
 
-        # Read file content and decode it (assuming it's a text file)
-        content = file.read().decode(
-            "utf-8"
-        )  # Adjust decoding if dealing with non-text files
+        content = file.read().decode("utf-8")
 
-        # Create a dictionary for the current file
         file_info = {"filename": filename, "fileExt": file_ext, "content": content}
 
-        # Add the file info dictionary to the list
         file_records.append(file_info)
 
-    # Call the function to save the files in the public folder
     write_to_file(file_records)
 
-    # Return the list of file records as a JSON response
     return {"files": file_records}, 200
 
 
