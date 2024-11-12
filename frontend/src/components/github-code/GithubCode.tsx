9a7e598ca0196761a51944104ace3dc274311569
adityanandanx <nandanaditya985@gmail.com>
2024-11-13 04:38:42 +0530

    chore: remove select

--- frontend/src/components/github-code/GithubCode.tsx
+++ frontend/src/components/github-code/GithubCode.tsx
@@ -1,22 +1,12 @@
 "use client";
 import { getGenerationFromGithubAction } from "@/lib/actions";
-import { supported_types } from "@/lib/config";
 import { downloadFile } from "@/lib/utils";
 import { useState } from "react";
-import { OutputType } from "../single-code/types";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select";
 
 const GithubCode = () => {
-  const [type, setType] = useState<OutputType>("code");
   const [isPending, setIsPending] = useState(false);
   const [url, setUrl] = useState("");
 
@@ -30,25 +20,10 @@ const GithubCode = () => {
           value={url}
           onChange={(e) => setUrl(e.target.value)}
         />
-        <Select
-          value={type}
-          onValueChange={(val) => setType(val as OutputType)}
-        >
-          <SelectTrigger className="capitalize w-full">
-            <SelectValue placeholder="Type" />
-          </SelectTrigger>
-          <SelectContent>
-            {supported_types.map((type) => (
-              <SelectItem className="capitalize" key={type} value={type}>
-                {type}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
         <Button
           onClick={async () => {
             setIsPending(true);
-            const blob = await getGenerationFromGithubAction(url, type);
+            const blob = await getGenerationFromGithubAction(url, "code");
 
             downloadFile(blob, "files.zip");
 
