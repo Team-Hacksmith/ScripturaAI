0b95a5802e1d798f06dc7446e13bd928534f233a
adityanandanx <nandanaditya985@gmail.com>
2024-11-12 13:24:32 +0530

    refactor: changes

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask, request
 from fileIO import write_to_file
-app = Flask(__name__)
 
 app = Flask(__name__)
 
 # Define the public directory to save files
-PUBLIC_DIR = 'public'
+PUBLIC_DIR = "uploads"
 
 # Ensure the public directory exists
 os.makedirs(PUBLIC_DIR, exist_ok=True)
+
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
@@ -26,11 +26,17 @@ def upload_file():
     # Process each file
     for file in files:
         # Extract the filename and file extension
-        filename = file.filename
+        if not file.filename:
+            print("Warning: No file name, skipping file...")
+            continue
+
+        filename: str = file.filename
         file_ext = filename.rsplit(".", 1)[-1] if "." in filename else ""
 
         # Read file content and decode it (assuming it's a text file)
-        content = file.read().decode("utf-8")  # Adjust decoding if dealing with non-text files
+        content = file.read().decode(
+            "utf-8"
+        )  # Adjust decoding if dealing with non-text files
 
         # Create a dictionary for the current file
         file_info = {"filename": filename, "fileExt": file_ext, "content": content}
@@ -43,7 +49,6 @@ def upload_file():
 
     # Return the list of file records as a JSON response
     return {"files": file_records}, 200
-
 
 
 if __name__ == "__main__":
